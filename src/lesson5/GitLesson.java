73cf33bee49a8f2b299b6b677a4ef0948fd45ded
v-lohvyniuk <volodymyr.lohvyniuk@gmail.com>
2025-11-02 21:49:07 +0200

    dummy update

--- src/lesson5/GitLesson.java
+++ src/lesson5/GitLesson.java
@@ -5,6 +5,8 @@ public class GitLesson {
     public static void main(String[] args) {
 
         System.out.println("This is updated version of the branch");
+
+        System.out.println("dummy update");
         int a = 4;
     }
 }
